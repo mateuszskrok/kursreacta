c561eff54086df7bb7763032c34472e01e0fd61f
Mateusz Skrok <m.p.skrok@gmail.com>
2020-04-03 14:16:34 +0200

    Move TimeboxCreator to separate file

--- src/components/remaining-components.js
+++ src/components/remaining-components.js
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 import Timebox from "./Timebox";
 import TimeboxEditor from "./TimeboxEditor";
+import TimeboxCreator from "./TimeboxCreator";
 import CurrentTimebox from "./CurrentTimebox";
 
 class EditableTimebox extends React.Component{
@@ -43,49 +44,6 @@ class EditableTimebox extends React.Component{
                     onEdit={this.handleEdit}
                     /> 
             </>
-        )
-    }
-}
-
-
-class TimeboxCreator extends React.Component{
-
-    constructor(props){
-        super(props);
-        this.titleInput = React.createRef();
-        this.totalTimeInMinutesInput = React.createRef();
-    }
-    handleSubmit = (event) => {
-        event.preventDefault(); 
-        this.props.onCreate({
-            id: uuidv4(), 
-            title: this.titleInput.current.value, 
-            totalTimeInMinutes: this.totalTimeInMinutesInput.current.value
-            });
-            this.titleInput.current.value = "";
-            this.totalTimeInMinutesInput.current.value = "";
-    }
-    render(){
-        return (
-        <form onSubmit={this.handleSubmit} className="TimeboxCreator">
-            <label>Co robisz?
-            <input
-                ref={this.titleInput}
-                type="text"
-            />
-            </label><br />
-            <label>Ile minut? 
-            <input 
-                ref={this.totalTimeInMinutesInput}
-                type="number"
-                
-            />
-            </label><br />
-            <button>
-                    Dodaj Timebox
-            </button>
-
-        </form>
         )
     }
 }
